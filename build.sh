ee07c1723243ff162860abb8a1b73a745455dc95
Hilman Kurniawan <hilman.k8n@gmail.com>
2024-07-17 23:55:28 +0700

    feat: add diff before build

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 #!/run/current-system/sw/bin/bash
 
+set -e
+
+printf "\nGit diff...\n"
+git diff
+
+printf "\nShowing meld..."
+meld configuration.nix /etc/nixos/configuration.nix
+meld hardware-configuration.nix /etc/nixos/hardware-configuration.nix
+
 cp configuration.nix /etc/nixos/configuration.nix
 cp hardware-configuration.nix /etc/nixos/hardware-configuration.nix
 nixos-rebuild switch
